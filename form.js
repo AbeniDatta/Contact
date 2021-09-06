5691de8224c22870a740364654ce0895010bd912
Abeni Datta <85713169+AbeniDatta@users.noreply.github.com>
2021-09-06 16:36:01 +0100

    Update form.js

--- form.js
+++ form.js
@@ -1,46 +1,56 @@
-var firebaseConfig = {
-    apiKey: "AIzaSyBYaSshT-5qPcCmVLLT-AoQzor6QBCqx4g",
-    authDomain: "abeni-s-website.firebaseapp.com",
-    projectId: "abeni-s-website",
-    storageBucket: "abeni-s-website.appspot.com",
-    messagingSenderId: "896410888982",
-    appId: "1:896410888982:web:9f82f923372f152ef8be37"
+const firebaseConfig = {
+  apiKey: "AIzaSyChZgHw903h8HVD8mdP8p8jR9EGlbt6UKg",
+  authDomain: "abeni-s-website-5b97c.firebaseapp.com",
+  projectId: "abeni-s-website-5b97c",
+  storageBucket: "abeni-s-website-5b97c.appspot.com",
+  messagingSenderId: "46093674097",
+  appId: "1:46093674097:web:cede493fe8bd236d8b9dd0"
 };
 // Init Firebase
 firebase.initializeApp(firebaseConfig);
-var firestore = firebase.firestore();
 
-// Start grabbing out DOM element
-const submitBtn = document.querySelector('#submit');
+//Reference for form collection(3)
+let formMessage = firebase.database().ref('ContactData');
 
-let userFirstName = document.querySelector('#userFirstName');
-let userLasttName = document.querySelector('#userLastName');
-let userEmail = document.querySelector('#userEmail');
-let userSubject = document.querySelector('#userSubject');
-let userMessage = document.querySelector('#userMessage');
+//listen for submit event//(1)
+document
+  .getElementById('my-form')
+  .addEventListener('submit', formSubmit);
 
-const db= firestore.collection("ContactData");
+//Submit form(1.2)
+function formSubmit(e) {
+  e.preventDefault();
+  // Get Values from the DOM
+  let firstname = document.querySelector('#userFirstName').value;
+  let lastname = document.querySelector('#userLastName').value;
+  let email = document.querySelector('#userEmail').value;
+  let subject = document.querySelector('#serSubject').value;
+  let message = document.querySelector('#userMessage').value;
 
-submitBtn.addEventListener('click',function(){
-    let userFirstNameInput = userFirstName.value;
-    let userLastNameInput = userLastName.value;
-    let userEmailInput = userEmail.value;
-    let userSubjectInput = userSubject.value;
-    let userMessageInput = userMessage.value;
+  //send message values
+  sendMessage(firstname, lastname, email, subject, message);
 
-    //Access the Database Collection
-    db.doc()
-    .set({
-        first_name: userFirstNameInput,
-        last_name: userLastNameInput,
-        email: userEmailInput,
-        subject: userSubjectInput,
-        message: userMessageInput
-    })
-    .then(function(){
-        console.log("Data Saved");
-    })
-    .catch(function(error){
-        console.log(error);
-    });
-});
+  //Show Alert Message(5)
+  document.querySelector('.alert').style.display = 'block';
+
+  //Hide Alert Message After Seven Seconds(6)
+  setTimeout(function() {
+    document.querySelector('.alert').style.display = 'none';
+  }, 7000);
+
+  //Form Reset After Submission(7)
+  document.getElementById('my-form').reset();
+}
+
+//Send Message to Firebase(4)
+
+function sendMessage(firstname, lastname, email, subject, message) {
+  let newFormMessage = formMessage.push();
+  newFormMessage.set({
+    First_Name: firstname,
+    Last_Name: lastname,
+    email: email,
+    subject: subject
+    message:message
+  });
+}
